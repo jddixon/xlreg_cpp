83487ddf47e191e9a44dd47fce1ce7f21dda55fc
Jim Dixon <jddixon@gmail.com>
2015-12-01 16:21:53 -0800

    minor edits to *.md; cleaned up autotoools stuff

--- src/version.cpp
+++ src/version.cpp
@@ -1,8 +1,8 @@
-// xlreg_cpp/src/version.cpp
+/* xlreg_cpp/src/version.cpp */
 
 #include <string>
 
 using namespace std;
 
-const std::string VERSION       = "0.0.3";
-const std::string VERSION_DATE  = "2015-10-11";
+const std::string *VERSION      = "0.0.4";
+const std::string *VERSION_DATE = "2015-12-01";
