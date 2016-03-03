12b13027ac6cf66b2391a795fd0dab838583f771
Jim Dixon <jddixon@gmail.com>
2016-03-03 13:12:18 -0800

    strip out autotools, cmake

--- src/version.cpp
+++ src/version.cpp
@@ -6,5 +6,5 @@
 
 using namespace std;
 
-const std::string VERSION      = "0.0.7";
-const std::string VERSION_DATE = "2016-03-01";
+const std::string VERSION      = "0.1.0";
+const std::string VERSION_DATE = "2016-03-03";
